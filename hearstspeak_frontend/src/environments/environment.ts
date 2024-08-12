f4e7c263dd68732323e9ead34831f6646f90c47b
ashokr <ashokr@hyperlinkinfosystem.net.in>
2024-08-12 18:39:32 +0530

    initial commit

--- hearstspeak_frontend/src/environments/environment.ts
+++ hearstspeak_frontend/src/environments/environment.ts
@@ -3,8 +3,7 @@
 
 export const environment = {
     production:true,
-    // API_URL:'http://192.168.1.231:9756/api/services/',
-    // API_URL:'http://192.168.1.231:9756/api/services/',
+  
   
     API_URL:'http://15.207.221.22:9756/api/services/',
   
